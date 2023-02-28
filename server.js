2231e11210a35c1920ff7e9722aeeeca6835add2
francesco.finotto <finoz.webdev@gmail.com>
2023-02-28 17:42:04 +0100

    notion integration - get detail, conversion in md + html

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const dotenv = require("dotenv");
 const cors = require("cors");
 const express = require("express");
-
-const { Client, APIErrorCode } = require("@notionhq/client");
+const { Client } = require("@notionhq/client");
+const { NotionToMarkdown } = require("notion-to-md");
+const showdown = require("showdown");
 
 dotenv.config();
 const server = express();
@@ -10,6 +11,7 @@ server.use(cors());
 const notion = new Client({
   auth: process.env.NOTION_RCP_TOKEN,
 });
+const n2m = new NotionToMarkdown({ notionClient: notion });
 
 server.get("/rcp/", async function (req, res) {
   const rcpDb = await notion.databases.query({
@@ -19,16 +21,22 @@ server.get("/rcp/", async function (req, res) {
 });
 
 server.get("/rcp/:id", async function (req, res) {
-  const rcpHeadings = await notion.pages.retrieve({
+  const notionHeadings = await notion.pages.retrieve({
     page_id: req.params.id,
   });
-  const rcpContent = await notion.blocks.children.list({
+
+  /* const rcpContent = await notion.blocks.children.list({
     block_id: req.params.id,
     page_size: 100,
-  });
+  }); */
+
+  const n2mBlocks = await n2m.pageToMarkdown(req.params.id);
+  const mdContent = n2m.toMarkdownString(n2mBlocks);
+  const converter = new showdown.Converter();
+  const content = converter.makeHtml(mdContent);
   const rcpData = {
-    properties: rcpHeadings,
-    content: rcpContent,
+    headings: notionHeadings,
+    content: content,
   };
   res.send(rcpData);
 });
--- src/scripts/components/notionListingGenerator.js
+++ src/scripts/components/notionListingGenerator.js
@@ -5,16 +5,80 @@ export default class NotionListingGenerator extends ListingGenerator {
     this.log = "Start finoz/notionListingGenerator";
   }
 
-  init() {
-    this.setData();
+  setApi() {
+    let baseurl = "http://localhost:3000";
+    return `${baseurl}/rcp/`;
   }
 
-  async setData() {
-    let data = await this.getData();
+  render() {
+    this.data.results.forEach((result) => {
+      let item = {
+        id: result.id,
+        title: result.properties.Name.title[0].plain_text,
+        created_time: result.created_time,
+        tags: result.properties.Tags.multi_select.map((tag) => tag.name),
+        notion_url: result.url,
+      };
+      let mkp = this.buildItem(item);
+      this.$el.insertAdjacentHTML("beforeend", mkp);
+    });
+    this.listenForDetail();
+  }
+
+  buildItem(data) {
+    let markup = "";
+    let itemclass = "item";
+    let fields = this.$options.itemfields
+      ? this.$options.itemfields.split(",")
+      : [];
+    markup += `<article data-id="${data.id}"`;
+    fields.forEach((field) => {
+      if (data[field]) {
+        let str = data[field];
+        str = str.toString().toLowerCase();
+        markup += ` data-${field}="${str}"`;
+      }
+    });
+    markup += ` class="${itemclass}">`;
+    markup += `<h2 class="${itemclass}-title">${data.title}</h2>`;
+    if (data.tags.length > 0) {
+      markup += `<ul class="${itemclass}-tags">`;
+      data.tags.forEach((tag) => {
+        markup += `<li>${tag}</li>`;
+      });
+      markup += `</ul>`;
+    }
+    markup += `<div class="${itemclass}-notion_url"><a href="${data.notion_url}" target="_blank">Notion</a></div>`;
+    markup += `</article>`;
+    return markup;
+  }
+
+  /*  */
+
+  listenForDetail() {
+    this.$el.querySelectorAll(".item").forEach((article) => {
+      article
+        .querySelector(".item-title")
+        .addEventListener("click", async (e) => {
+          let { headings, content } = await this.getDetailData(
+            article.dataset.id
+          );
+          this.buildHeading(headings);
+          this.buildContent(content);
+        });
+    });
+  }
+
+  async getDetailData(id) {
+    let api = `${this.api}${id}`;
+    return await fetch(api).then((response) => response.json());
+  }
+
+  buildHeading(data) {
     console.log(data);
   }
-  async getData() {
-    let baseurl = "http://localhost:3000";
-    return await fetch(`${baseurl}/rcp/`).then((res) => res.json());
+
+  buildContent(data) {
+    console.log(data);
   }
 }
